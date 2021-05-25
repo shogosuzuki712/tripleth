08e8e9874ae3642dc961a6a26ea13276b4526e0f
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-05-25 19:36:10 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'homes#index'
+  resources :users, only: [:show, :edit]
+  resources :tweets, only: [:index, :new, :show, :delete]
+  resources :meals, only: [:index, :new, :show, :delete]
+  resources :trainings, only: [:index, :new, :show, :delete]
+  resources :processes, only: [:index, :new, :show, :delete]
+  resources :records, only: [:index]
+  resources :in_takes, only: [:new, :show, :delete]
+  resources :out_takes, only: [:new, :show, :delete]
+  resources :checks, only: [:new, :show, :delete]
 end
