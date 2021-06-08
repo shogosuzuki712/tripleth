da1642a7834cbed23e8f90425438c170fc18d2ff
yudai-simoma <cdcdasdll@icloud.com>
2021-06-08 21:53:43 +0900

    processesをsuccessesに変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :tweets, only: [:index, :new, :show, :delete]
   resources :meals, only: [:index, :new, :show, :delete]
   resources :trainings, only: [:index, :new, :show, :delete]
-  resources :processes, only: [:index, :new, :show, :delete]
+  resources :successes, only: [:index, :new, :show, :delete]
   resources :records, only: [:index]
   resources :in_takes, only: [:new, :show, :delete]
   resources :out_takes, only: [:new, :show, :delete]
