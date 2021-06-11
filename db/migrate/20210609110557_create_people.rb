ca0e55b737a3e8c64cf90bd0658b2b3bfec24e3d
shomaaan <80161060+shomaaan@users.noreply.github.com>
2021-06-11 17:50:11 +0900

    カラム名変更につきマイグレーションファイル編集

--- db/migrate/20210609110557_create_people.rb
+++ db/migrate/20210609110557_create_people.rb
@@ -1,9 +1,9 @@
 class CreatePeople < ActiveRecord::Migration[6.0]
   def change
     create_table :people do |t|
-      t.integer :height
-      t.integer :weight
-      t.integer :goal
+      t.integer    :height, null: false
+      t.integer    :weight, null: false
+      t.integer    :goal, null: false
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
