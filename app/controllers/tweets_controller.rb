502db6b417bc53b6a80f0960f0712aba2b7babf1
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-06-14 19:15:53 +0900

    actiontext実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,14 +3,26 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def delete
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:tweet)
   end
 end
