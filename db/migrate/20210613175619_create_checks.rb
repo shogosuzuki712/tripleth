dea6a46fca6b02c69f1084c188a595dc21a0b847
yudai-simoma <cdcdasdll@icloud.com>
2021-06-16 22:44:34 +0900

    checksテーブル作成

--- db/migrate/20210613175619_create_checks.rb
+++ db/migrate/20210613175619_create_checks.rb
@@ -1,7 +1,9 @@
 class CreateChecks < ActiveRecord::Migration[6.0]
   def change
     create_table :checks do |t|
-
+      t.integer :weight, null: false
+      t.datetime :start_time, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
