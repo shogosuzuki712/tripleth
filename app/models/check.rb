cc208b6a7e97e6aed5a0f7daa9ed94b02c310480
yudai-simoma <cdcdasdll@icloud.com>
2021-06-16 22:44:49 +0900

    checkモデル作成

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,2 +1,10 @@
 class Check < ApplicationRecord
+  with_options presence: true do
+    validates :weight
+    validates :image
+    validates :start_time
+  end
+
+  belongs_to :user
+  has_one_attached :image
 end
