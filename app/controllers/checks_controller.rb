7feff394e035084923b268c8f898d9631fdea1a3
yudai-simoma <cdcdasdll@icloud.com>
2021-06-21 21:00:06 +0900

    グラフ表示機能の実装

--- app/controllers/checks_controller.rb
+++ app/controllers/checks_controller.rb
@@ -2,6 +2,15 @@ class ChecksController < ApplicationController
   def new
     @checks = Check.all
     @check = Check.new
+
+    # @graph = Check.where(user_id: current_user.id).limit(3).order(weight: "ASC"))
+    # @graph = Check.all
+    
+    # グラフの基準線作成用の配列
+    @data = [['2019-06-01', 100], ['2019-06-02', 200], ['2019-06-03', 150]]
+    # @graph.each do |rec|
+    #   @donation_days << rec.weight
+    # end
   end
 
   def create
