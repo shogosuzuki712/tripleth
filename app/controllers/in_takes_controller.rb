bc4ff24c4aec1359b698d2e8047483ec20ef216a
shotaro-arai <syoooth0720@yahoo.co.jp>
2021-07-07 20:19:10 +0900

    複数レコードを同時登録できるようcreateアクションを設定

--- app/controllers/in_takes_controller.rb
+++ app/controllers/in_takes_controller.rb
@@ -1,8 +1,24 @@
 class InTakesController < ApplicationController
   def new
+    @in_take = InTake.new
   end
 
   def create
+    if params[:food_category_id] && params[:food_id] && params[:food_category_id].length == params[:food_id].length
+      c = 0
+      params[:food_category_id].length.times do |c| #データ登録分繰り返す
+        @in_take = InTake.new(in_take_params)  #start_timeとuser_idのみ入ったインスタンス生成
+        @in_take.food_category_id = params[:food_category_id][c]  #配列c番目のfood_category_idを付与
+        @in_take.food_id = params[:food_id][c]  #配列c番目のfood_idを付与
+        @in_take.save  #c番目のデータ登録
+        c += 1  #cに1追加する
+      end
+      redirect_to new_in_take_path
+    else
+      @in_take = InTake.new(in_take_params)
+      @in_take.valid?
+      render "new"
+    end
   end
 
   def show
@@ -10,4 +26,11 @@ class InTakesController < ApplicationController
 
   def delete
   end
+
+
+  private 
+  def in_take_params
+    params.require(:in_take).permit(:start_time).merge(user_id: params[:format])
+  end
+  
 end
