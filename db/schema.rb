14adb40d7bea92770c3f6d93f3cc538d55a259b9
yudai-simoma <cdcdasdll@icloud.com>
2021-09-05 19:14:57 +0900

    diet_menuモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_13_175619) do
+ActiveRecord::Schema.define(version: 2021_06_22_145723) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,15 @@ ActiveRecord::Schema.define(version: 2021_06_13_175619) do
     t.index ["user_id"], name: "index_checks_on_user_id"
   end
 
+  create_table "diet_menus", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.integer "genre_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diet_menus_on_user_id"
+  end
+
   create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "height", null: false
     t.integer "weight", null: false
@@ -98,6 +107,7 @@ ActiveRecord::Schema.define(version: 2021_06_13_175619) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "charas", "users"
   add_foreign_key "checks", "users"
+  add_foreign_key "diet_menus", "users"
   add_foreign_key "people", "users"
   add_foreign_key "tweets", "users"
 end
