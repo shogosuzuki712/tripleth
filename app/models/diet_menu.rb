8e48a32a084b5a49db33b5d87246e2d307eb962c
yudai-simoma <cdcdasdll@icloud.com>
2021-09-05 19:18:23 +0900

    Revert "diet_menuモデルの作成"
    This reverts commit 14adb40d7bea92770c3f6d93f3cc538d55a259b9.

--- app/models/diet_menu.rb
+++ app/models/diet_menu.rb
@@ -1,10 +1,2 @@
 class DietMenu < ApplicationRecord
-  validates :title, presence: true
-  validates :genre_id, numericality: { other_than: 1 }
-
-  belongs_to :user
-  has_rich_text :text
-  has_one_attached :image
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
 end
--- db/migrate/20210622145723_create_diet_menus.rb
+++ db/migrate/20210622145723_create_diet_menus.rb
@@ -1,9 +1,7 @@
 class CreateDietMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :diet_menus do |t|
-      t.string :title, nill: false
-      t.integer :genre_id, null:false
-      t.references :user,null: false, foreign_key: true
+
       t.timestamps
     end
   end
