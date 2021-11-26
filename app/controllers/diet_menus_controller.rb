e12cac23eb4f155295ceb5ac1cbd547906abfdfc
yudai-simoma <cdcdasdll@icloud.com>
2021-11-27 06:45:35 +0900

    登録機能作成

--- app/controllers/diet_menus_controller.rb
+++ app/controllers/diet_menus_controller.rb
@@ -3,14 +3,25 @@ class DietMenusController < ApplicationController
   end
 
   def new
+    @diet_menu = Diet_menu.new
   end
 
   def create
+    @diet_menu = Diet_menu.new(dien_menu_params)
+    if @diet_menu.save
+      redirect_to diet_menus_path
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def delete
+  end
+
+  def dien_menu_params
+    params.require(:diet_menu).permit(:genre_id, :title, :content).merge(user_id: current_user.id)
   end
 end
